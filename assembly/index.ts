2265fc2614cce428109e82518a4d94aab1ae5f1d
Guillaume Guimond <guillaume.guimond@orange.com>
2022-10-20 17:47:36 +0200

    added 2 methods to create checker board using array and multidmensional array

--- assembly/index.ts
+++ assembly/index.ts
@@ -6,7 +6,7 @@ export function add(a: i32, b: i32): i32 {
 
 //RAW memory example
 memory.grow(1)
-const CHECKERBOARD_SIZE: i32 = 20
+export const CHECKERBOARD_SIZE: i32 = 20
 export const CHECKERBOARD_BUFFER_POINTER: i32 = 0
 export const CHECKERBOARD_BUFFER_SIZE: i32 = CHECKERBOARD_SIZE * CHECKERBOARD_SIZE * 4
 
@@ -57,4 +57,97 @@ export function generateCheckerBoard(
       store<u8>(CHECKERBOARD_BUFFER_POINTER + squareRgbaIndex + 3, 255) // Alpha (Always opaque)
     }
   }
+}
+
+
+//array example
+export function generateCheckerBoardArray(
+  darkValueRed: i32,
+  darkValueGreen: i32,
+  darkValueBlue: i32,
+  lightValueRed: i32,
+  lightValueGreen: i32,
+  lightValueBlue: i32
+): Int32Array {
+  const checkBoard = new Int32Array(CHECKERBOARD_BUFFER_SIZE)
+  for (let x: i32 = 0; x < CHECKERBOARD_SIZE; x++) {
+    for (let y: i32 = 0; y < CHECKERBOARD_SIZE; y++) {
+
+      let isDarkSquare: boolean = true
+      if (y % 2 === 0) {
+        isDarkSquare = false;
+      }
+      if (x % 2 === 0) {
+        isDarkSquare = !isDarkSquare;
+      }
+
+      let squareValueRed = darkValueRed;
+      let squareValueGreen = darkValueGreen;
+      let squareValueBlue = darkValueBlue;
+      if (!isDarkSquare) {
+        squareValueRed = lightValueRed;
+        squareValueGreen = lightValueGreen;
+        squareValueBlue = lightValueBlue;
+      }
+
+      let squareNumber = y * CHECKERBOARD_SIZE + x
+      let squareRgbaIndex = squareNumber * 4
+
+      checkBoard[squareRgbaIndex] = squareValueRed
+      checkBoard[squareRgbaIndex + 1] = squareValueGreen
+      checkBoard[squareRgbaIndex + 2] = squareValueBlue
+      checkBoard[squareRgbaIndex + 3] = 255
+
+    }
+  }
+  return checkBoard
+
+}
+
+//array example
+export function generateCheckerBoard2dArray(
+  darkValueRed: i32,
+  darkValueGreen: i32,
+  darkValueBlue: i32,
+  lightValueRed: i32,
+  lightValueGreen: i32,
+  lightValueBlue: i32
+): Array<Array<Int32Array>> {
+  const checkBoard = new Array<Array<Int32Array>>(CHECKERBOARD_BUFFER_SIZE)
+  for (let x: i32 = 0; x < CHECKERBOARD_SIZE; x++) {
+    checkBoard[x] = new Array<Int32Array>(CHECKERBOARD_SIZE)
+    for (let y: i32 = 0; y < CHECKERBOARD_SIZE; y++) {
+
+      let isDarkSquare: boolean = true
+      if (y % 2 === 0) {
+        isDarkSquare = false;
+      }
+      if (x % 2 === 0) {
+        isDarkSquare = !isDarkSquare;
+      }
+
+      let squareValueRed = darkValueRed;
+      let squareValueGreen = darkValueGreen;
+      let squareValueBlue = darkValueBlue;
+      if (!isDarkSquare) {
+        squareValueRed = lightValueRed;
+        squareValueGreen = lightValueGreen;
+        squareValueBlue = lightValueBlue;
+      }
+
+      let squareNumber = y * CHECKERBOARD_SIZE + x
+      let squareRgbaIndex = squareNumber * 4
+
+      const entry = new Int32Array(4)
+
+      entry[0] = squareValueRed
+      entry[1] = squareValueGreen
+      entry[2] = squareValueBlue
+      entry[3] = 255
+
+      checkBoard[x][y] = entry
+    }
+  }
+  return checkBoard
+
 }
--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,5 @@
 import assert from "assert"
-import { add, generateCheckerBoard, memory, CHECKERBOARD_BUFFER_POINTER, CHECKERBOARD_BUFFER_SIZE } from "../build/debug.js"
+import { add, generateCheckerBoard, memory, CHECKERBOARD_BUFFER_POINTER, CHECKERBOARD_BUFFER_SIZE, CHECKERBOARD_SIZE, generateCheckerBoardArray, generateCheckerBoard2dArray } from "../build/debug.js"
 assert.strictEqual(add(1, 2), 3)
 
 generateCheckerBoard(1, 2, 3, 4, 5, 6)
@@ -17,4 +17,27 @@ assert.strictEqual(imageDataArray[4],4)
 assert.strictEqual(imageDataArray[5],5)
 assert.strictEqual(imageDataArray[6],6)
 assert.strictEqual(imageDataArray[7],255)
+
+const imageDataArray2 = generateCheckerBoardArray(1, 2, 3, 4, 5, 6)
+assert.strictEqual(imageDataArray2.length, CHECKERBOARD_BUFFER_SIZE.valueOf())
+assert.strictEqual(imageDataArray2[0],1)
+assert.strictEqual(imageDataArray2[1],2)
+assert.strictEqual(imageDataArray2[2],3)
+assert.strictEqual(imageDataArray2[3],255)
+assert.strictEqual(imageDataArray2[4],4)
+assert.strictEqual(imageDataArray2[5],5)
+assert.strictEqual(imageDataArray2[6],6)
+assert.strictEqual(imageDataArray2[7],255)
+
+const imageDataArray3 = generateCheckerBoard2dArray(1, 2, 3, 4, 5, 6)
+assert.strictEqual(imageDataArray3.length, CHECKERBOARD_BUFFER_SIZE.valueOf())
+assert.strictEqual(imageDataArray3[0][0][0],1)
+assert.strictEqual(imageDataArray3[0][0][1],2)
+assert.strictEqual(imageDataArray3[0][0][2],3)
+assert.strictEqual(imageDataArray3[0][0][3],255)
+assert.strictEqual(imageDataArray3[0][1][0],4)
+assert.strictEqual(imageDataArray3[0][1][1],5)
+assert.strictEqual(imageDataArray3[0][1][2],6)
+assert.strictEqual(imageDataArray3[0][1][3],255)
+
 console.log("ok")
